2cc2656537c376d8cbb47ae50cae816db59461de
Jarlath <jarlath@jarlath.cc>
2025-04-24 23:47:39 -0500

    Add jquery code to myscript.js and styles within style tags to table.html

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,8 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+    // $('h2').css('text-decoration', 'underline');
+    $('h2').addClass('underline');
+    $('h2').removeClass('underline');
+    // $('ul').css('border', 'solid 1px #ccc');
+    $('ul').addClass('border');
+    $('ul').removeClass('border');
+});
